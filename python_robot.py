e6c1a067cefc64ae8e12e39820231d895f408547
ramona <ramona.racz@ausy.com>
2023-03-30 10:07:48 +0300

    Implemented the function extract_text

--- python_robot.py
+++ python_robot.py
@@ -20,6 +20,20 @@ def received_text():
                 stored_info(text_line)
 
 def extract_text():
+    for line in stored_info:
+        time = re.search(r'\d{2}- \d{2}) \d{2}:\d{2}:\d{2}\. \d{3}', line)
+        package = re.search("cmp=([\w\./]+)", line)
+        start_app_date = time.group(0)
+        if package and time:
+            app = 'application{}'.format(len(extract_info) + 1)
+            app_path = package.group(1)
+            extract_info[app] = {"app_path", 'ts_app_started': start_android_app}
+        if "/" in app_path:
+            apps_value = app_path.split('/')[0]
+            if apps_value and stop_android_app in line:
+                stop = time.group(0)
+                extract_info[app]['ts_app_closed'] = stop
+
 
 
 if __name__ == "__main__":
