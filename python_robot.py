9ab1cf996137ae869f42951577674fa53c9e3e43
ramona <ramona.racz@ausy.com>
2023-04-06 16:27:16 +0300

    I have implemented lifespan

--- python_robot.py
+++ python_robot.py
@@ -1,42 +1,51 @@
-# def received_text():
-# file = open("logcat_applications.txt", 'rt')
-# file_content = file.read()
-# print(file_content)
-# file.close()
-
 import re
+from pprint import pprint
+from datetime import datetime
+
 
 start_android_app = "ActivityTaskManager: START u0"
 stop_android_app = "Layer: Destroyed ActivityRecord"
 stored_info = []
 extract_info = {}
 
+
 def received_text():
     with open("logcat_applications.txt", "rt") as file:
         for text_line in file:
             if start_android_app in text_line:
                 stored_info.append(text_line)
-            if stop_android_app in text_line:
-                stored_info(text_line)
+            elif stop_android_app in text_line:
+                stored_info.append(text_line)
+
 
 def extract_text():
     for line in stored_info:
-        time = re.search(r'\d{2}- \d{2}) \d{2}:\d{2}:\d{2}\. \d{3}', line)
-        package = re.search("cmp=([\w\./]+)", line)
-        start_app_date = time.group(0)
+        time = re.search(r'\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}', line)
+        # time = re.search(r'\d{2}- \d{2} \d{2}:\d{2}:\d{2}\. \d{3}', line)  # print time
+        # package = re.search("cmp=([\w\./]+)", line)  # print package
+        package = re.search('cmp=([\w\./]+)', line)
+        start_app = time.group(0)
+
         if package and time:
-            app = 'application{}'.format(len(extract_info) + 1)
             app_path = package.group(1)
-            extract_info[app] = {"app_path", 'ts_app_started': start_android_app}
-        if "/" in app_path:
-            apps_value = app_path.split('/')[0]
-            if apps_value and stop_android_app in line:
-                stop = time.group(0)
-                extract_info[app]['ts_app_closed'] = stop
+            app = 'application_{}'.format(len(extract_info) + 1)
+            extract_info[app] = {"app_path": app_path, 'ts_app_started': start_app, 'ts_app_closed': None, 'lifespan': None}
 
+        else:
+            if stop_android_app in line:
+                pack = re.search(r"com.([\w.]+)", line)
+                stop_date = time.group(0)
+                path = pack.group(0)
+                for applications in extract_info.items():
+                    if path in applications[1]['app_path']:
+                        applications[1]['ts_app_closed'] = stop_date
+                        time_stop = datetime.strptime(stop_date, '%m-%d %H:%M:%S.%f')
+                        time_start = datetime.strptime(applications[1]['ts_app_started'], '%m-%d %H:%M:%S.%f')
+                        lifespan = time_stop - time_start
+                        applications[1]['lifespan'] = str(lifespan.total_seconds()) + "s"
 
+    pprint(extract_info)
 
 if __name__ == "__main__":
     received_text()
-
-
+    extract_text()
